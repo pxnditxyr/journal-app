320859a7dfe9bcc23087258e8c2e6e45dfcd8500
Yuki XD <pxnditxyr@gmail.com>
2021-11-28 00:12:50 -0400

    styles[src]: Added new basic setting for page styles with sass, and imported in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import ReactDOM from 'react-dom';
 
 import { Journal } from './journal/Journal';
 
+import './journal/assets/styles/styles.scss';
+
 ReactDOM.render(
     <Journal />,
     document.querySelector( "#root" ),
