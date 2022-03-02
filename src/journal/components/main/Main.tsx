f61fcbd69bb8c5a2fb1428e802d0c685fab6b3fb
Yuki xD <pxnditxyr@gmail.com>
2022-03-02 13:06:59 -0400

    feat[main]: Added state reading for display note or nothing selected

--- src/journal/components/main/Main.tsx
+++ src/journal/components/main/Main.tsx
@@ -1,14 +1,26 @@
+import { useSelector } from 'react-redux';
 import { Sidebar } from '../sidebar/Sidebar';
-// import { NothingSelected } from '../nothing-selected/NothingSelected';
+import { NothingSelected } from '../nothing-selected/NothingSelected';
 import { NoteScreen } from '../notes/NoteScreen';
 
+import { State } from '../../interfaces/interfaces';
+
 export const Main = () => {
+
+    const { activeNote } = useSelector( ( state : State ) => state.notes );
+
+    // console.log( activeNote );
+
     return (
         <>
             <Sidebar />
 
             <main>
-                <NoteScreen />
+                { 
+                    ( activeNote )
+                        ? <NoteScreen />
+                        : <NothingSelected />
+                }
             </main>
         </>
     );
