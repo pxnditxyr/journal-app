537b00487effff263af68519e988558ded308423
Yuki xD <pxnditxyr@gmail.com>
2022-03-02 11:53:01 -0400

    chore[main]: Deleted component nothing selected commented

--- src/journal/components/main/Main.tsx
+++ src/journal/components/main/Main.tsx
@@ -1,5 +1,5 @@
 import { Sidebar } from '../sidebar/Sidebar';
-import { NothingSelected } from '../nothing-selected/NothingSelected';
+// import { NothingSelected } from '../nothing-selected/NothingSelected';
 import { NoteScreen } from '../notes/NoteScreen';
 
 export const Main = () => {
@@ -8,10 +8,7 @@ export const Main = () => {
             <Sidebar />
 
             <main>
-
-                {/* <NothingSelected /> */}
                 <NoteScreen />
-
             </main>
         </>
     );
